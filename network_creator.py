a668aaa1a94484324de53e897ed63a2d6cd0ad46
Dvir <dvir.cohn@gmail.com>
2017-05-07 15:42:52 +0300

    merged

--- network_creator.py
+++ network_creator.py
@@ -58,6 +58,7 @@ def main():
     number_of_walks = 5
     length_of_walks = 5
     window_size = 2
+    num_of_iteration = 2000
     csv_path = "C:\Users\Dvir\Desktop\NNftw\words2.csv"
 
     # make graph
@@ -81,14 +82,14 @@ def main():
 
     # make the random walks
     random_walks = make_random_walks(adj_matrix, number_of_walks, length_of_walks)
-    # save_to_csv(random_walks, csv_path)
 
     print "The input to wevi:"
-    print wevi_parser(random_walks, window_size)
+    after_parse =  wevi_parser(random_walks, window_size)
+    print after_parse
+    centrality_vector = wevi_automate(after_parse, num_of_iteration)
     print "Please paste here the results from wevi"
-    input1 = raw_input()
 
-    centrality_compare(graph, input1)
+    centrality_compare(graph, centrality_vector)
 
 if __name__ == "__main__":
     main()
